f0a63b8d3f3b5b71c3919aa1761573285b387598
Nathan Robertson <nathanrobertson1997@gmail.com>
2018-05-15 10:16:05 +1000

    Added the GUI window.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+//Imports
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
+
+	    //Create the window
+        JFrame canvas = new JFrame();
+        //Stop the app when the window is closed
+        canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Set the window size
+        canvas.setSize(500,500);
+        //Set the window tittle
+        canvas.setTitle("Education Industry Training System (EITS)");
+        //Set the window to visible
+        canvas.setVisible(true);
     }
 }
