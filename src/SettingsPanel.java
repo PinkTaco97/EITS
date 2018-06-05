285cbe84101d2ff3de472ca2fdd1f3369f933b3e
Liana Taylor <lianat@me.com>
2018-06-05 10:51:07 +1000

    Added Account Settings Panel, Updated Settings GUI.

--- src/SettingsPanel.java
+++ src/SettingsPanel.java
@@ -5,6 +5,16 @@ public class SettingsPanel {
 
     //Components
     public static JPanel panel = new JPanel();
+    public static JLabel title = new JLabel("Account Settings", JLabel.CENTER);
+    public static JLabel usernameText = new JLabel("Username:");
+    public static JTextField usernameInput = new JTextField();
+    public static JLabel passwordText = new JLabel("Password:");
+    public static JTextField passwordInput = new JTextField();
+    public static JLabel emailText = new JLabel("Email:");
+    public static JTextField emailInput = new JTextField();
+
+
+
 
     //Fonts
     public static final String fontFamily = "Apple Casual";
@@ -30,10 +40,45 @@ public class SettingsPanel {
         panel.setBackground(backgroundColor);
         panel.setVisible(false);
 
+        //Title
+        title.setBounds(0, 25, 900, 75);
+        title.setFont(h1);
+
+        //Username Text
+        usernameText.setBounds(250, 125, 400, 50);
+        usernameText.setFont(h2);
+
+        //Username Input
+        usernameInput.setBounds(250, 175, 400, 50);
+        usernameInput.setFont(h2);
+
+        //Password Text
+        passwordText.setBounds(250, 250, 400, 50);
+        passwordText.setFont(h2);
+
+        //Password Input
+        passwordInput.setBounds(250, 300, 400, 50);
+        passwordInput.setFont(h2);
+
+        //Email Text
+        emailText.setBounds(250, 375, 400, 50);
+        emailText.setFont(h2);
+
+        //Email Font
+        emailInput.setBounds(250, 425, 400, 50);
+        emailInput.setFont(h2);
+
     }
 
     //Add the Components
     public static void addComponents(){
+        panel.add(title);
+        panel.add(usernameText);
+        panel.add(usernameInput);
+        panel.add(passwordText);
+        panel.add(passwordInput);
+        panel.add(emailText);
+        panel.add(emailInput);
         panel.setLayout(new BorderLayout());
     }
 }
