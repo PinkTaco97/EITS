15ce69ceab91507f5941f4ddc8ebfc4a8dedd489
Nathan Robertson <nathanrobertson1997@gmail.com>
2018-06-12 09:07:54 +1000

    Updated SQL.

--- EITS.sql
+++ EITS.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 17, 2018 at 02:12 PM
+-- Generation Time: Jun 12, 2018 at 09:05 AM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -22,19 +22,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Courses` (
   `ID` int(11) NOT NULL,
-  `Code` varchar(255) NOT NULL,
   `Name` varchar(255) NOT NULL,
-  `Level` varchar(255) NOT NULL,
   `IndustryID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `Courses`
---
-
-INSERT INTO `Courses` (`ID`, `Code`, `Name`, `Level`, `IndustryID`) VALUES
-(1, 'ICT369', 'Diploma of Software Development', 'Diploma', 1),
-(2, 'ICT583', 'Information Digital Media and Technology', 'Cert III', 1);
 
 -- --------------------------------------------------------
 
@@ -46,14 +36,6 @@ CREATE TABLE `Industry` (
   `ID` int(11) NOT NULL,
   `Name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `Industry`
---
-
-INSERT INTO `Industry` (`ID`, `Name`) VALUES
-(1, 'Information Communication Technology'),
-(2, 'Accounting');
 
 -- --------------------------------------------------------
 
@@ -68,27 +50,6 @@ CREATE TABLE `Units` (
   `CourseID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `Units`
---
-
-INSERT INTO `Units` (`ID`, `Code`, `Description`, `CourseID`) VALUES
-(1, 'ICT001', 'Contribute to copyright, ethics and privacy in an ICT environment.', 1),
-(2, 'ICT002', 'Apply intermediate programming skills in another language.', 1),
-(4, 'ICT003', 'Apply advanced object oriented language skills.', 1),
-(5, 'ICT004', 'Manage a project using software management tools.', 1),
-(6, 'ICT005', 'Debug and monitor applications.', 1),
-(7, 'ICT006', 'Deploy an application to a production environment.', 1),
-(8, 'ICT007', 'Validate an application design against specifications.', 1),
-(9, 'ICT008', 'Apply advanced programming skills in another language.', 1),
-(10, 'ICT009', 'Apply testing techniques for software development.', 1),
-(11, 'ICT010', 'Participate effectively in WHS communication and consultation processes. ', 2),
-(12, 'ICT011', 'Implement and monitor environmentally sustainable work practices.', 2),
-(13, 'ICT012', 'Work and communicate effectively in an ICT environment.', 2),
-(14, 'ICT013', 'Create user documentation.', 2),
-(15, 'ICT014', 'Install and optimize operating system software.', 2),
-(16, 'ICT015', 'Run standard diagnostic tests.', 2);
-
 -- --------------------------------------------------------
 
 --
@@ -100,9 +61,22 @@ CREATE TABLE `Users` (
   `Username` varchar(255) NOT NULL,
   `Password` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
-  `CourseID` int(11) NOT NULL,
-  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `CourseID` int(11) DEFAULT NULL,
+  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Access` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`ID`, `Username`, `Password`, `Email`, `CourseID`, `Time`, `Access`) VALUES
+(1, 'admin', 'admin', 'admin@example.com', 1, '2018-06-11 23:00:45', 0),
+(2, 'Nathan', 'nathan', 'nathan@example.com', NULL, '2018-06-11 23:01:39', 2),
+(3, 'Liana', 'liana', 'liana@example.com', NULL, '2018-06-11 23:01:55', 2),
+(4, 'Kaitlyn', 'kaitlyn', 'kaitlyn@example.com', NULL, '2018-06-11 23:02:14', 2),
+(5, 'Rani', 'rani', 'rani@example.com', NULL, '2018-06-11 23:02:43', 2),
+(6, 'staff', 'staff', 'staff@example.com', NULL, '2018-06-11 23:03:14', 1);
 
 --
 -- Indexes for dumped tables
@@ -140,22 +114,22 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for table `Courses`
 --
 ALTER TABLE `Courses`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Industry`
 --
 ALTER TABLE `Industry`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Units`
 --
 ALTER TABLE `Units`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
